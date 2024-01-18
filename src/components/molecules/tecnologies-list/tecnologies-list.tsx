7c8dde98d3f9a48bf4228dee15cb6593c58e38ce
Julia Santi <juliagabriela98@gmail.com>
2024-01-18 08:03:04 -0300

    fix: hide technologies images

--- src/components/molecules/tecnologies-list/tecnologies-list.tsx
+++ src/components/molecules/tecnologies-list/tecnologies-list.tsx
@@ -3,33 +3,31 @@ import styles from "./tecnologies-list.module.css";
 import { TTecnologiesList } from "./tecnologies-list.types";
 
 const TechnologyList = ({
-    technologies,
-    onTechnologyClick,
-    currentTechnology,
+  technologies,
+  onTechnologyClick,
+  currentTechnology,
 }: TTecnologiesList) => {
-    return (
-        <ul className={styles.technologiesList}>
-            {technologies.map((technology) => (
-                <li
-                    key={technology.id}
-                    className={`${styles.technologyListItem} ${
-                        technology === currentTechnology
-                            ? styles.technologyActive
-                            : ""
-                    }`}
-                    onClick={() => onTechnologyClick(technology)}
-                >
-                <img
+  return (
+    <ul className={styles.technologiesList}>
+      {technologies.map((technology) => (
+        <li
+          key={technology.id}
+          className={`${styles.technologyListItem} ${
+            technology === currentTechnology ? styles.technologyActive : ""
+          }`}
+          onClick={() => onTechnologyClick(technology)}
+        >
+          {/*<img
                     width={currentTechnology?.width ?? 25}
                     height={25}
                     src={technology.image}
                     alt={technology.label}
-                />
-                    {technology.label}
-                </li>
-            ))}
-        </ul>
-    );
+                />*/}
+          {technology.label}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TechnologyList;
