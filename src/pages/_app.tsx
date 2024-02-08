bfc2b452d179d5d8c08d98fe170b1f4981bb337e
Julia Santi <juliagabriela98@gmail.com>
2024-02-08 00:22:10 -0300

    fix: title and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Júlia</title>
+        <link rel="icon" href="/profile-cartoon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
