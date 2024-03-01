f8a0012af23dc54715a03ec17e39fda394d992a7
Julia Santi <juliagabriela98@gmail.com>
2024-02-29 22:09:20 -0300

    fix: about mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ import { Suspense, useState } from "react";
 import { Sky } from "@/models/sky";
 
 export default function Home() {
-  const [isRotating, setIsRotating] = useState(false);
   const [activeSection, setActiveSection] = useState("frontend");
   const handleSectionClick = (section: string) => {
     setActiveSection(section);
@@ -26,23 +25,17 @@ export default function Home() {
       <Projects activeSection={activeSection} />
       <Experience />
       <Mentoring />
-      <div>
+      <div style={{ position: "relative" }}>
         <About />
+        <Canvas
+          className={styles.backgroundAnimation}
+          camera={{ near: 0.1, far: 1000 }}
+        >
+          <Suspense>
+            <Sky isRotating={true} />
+          </Suspense>
+        </Canvas>
       </div>
-      <Canvas
-        style={{
-          zIndex: "-2",
-          width: "100%", // Correspondente a w-full do Tailwind
-          height: "100vh", // Correspondente a h-screen do Tailwind
-          backgroundColor: "transparent", // Correspondente a bg-transparent do Tailwind
-          cursor: isRotating ? "grabbing" : "grab",
-        }}
-        camera={{ near: 0.1, far: 1000 }}
-      >
-        <Suspense>
-          <Sky isRotating={true} />
-        </Suspense>
-      </Canvas>
     </main>
   );
 }
